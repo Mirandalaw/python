e88d296fb44abe8488f072cc77ceb434299b7e0e
Mirandalaw <shrup5@naver.com>
2021-03-08 11:12:05 +0900

    eval 이란?

--- python.py
+++ python.py
@@ -1,13 +1,17 @@
-import random
+##변수 선언 부분##
+select, answer, numStr,num1,num2=0,0,"",0,0
 
-numbers=[]
-for num in range(0,10):
-    numbers.append(random.randrange(0,10)) #append는 리스트에 뭔가를 더 추가함.
+select=int(input("1. 입력한 수식 계산 2.두 수 사이의 합계 : "))
 
-print("생성된 리스트",numbers)
-
-for num in range(0,10):
-    if num not in numbers:
-        print("숫자 %d는 리스트에 없네요^^"%num)
-        #if 항목  in 리스트 : 는 리스트에 해당 항목이 있다면 True를 반환
-        #if 항목 not in 리스트 : 는 리스트에 항목이 없어야 True를 반환
+if select==1:
+    numStr=input("***수식을 입력하세요 : ")
+    answer=eval(numStr)#eval : (수식) 함수로 수식을 계산한다.
+    print("%s 결과는 %5.1f입니다. "%(numStr,answer))
+elif select==2:
+    num1=int(input("***첫 번째 숫자를 입력하세요 : "))
+    num2= int(input("***두 번째 숫자를 입력하세요 : "))
+    for i in range(num1,num2+1):
+        answer+=i
+    print("%d+...+%d는 %d입니다."%(num1,num2,answer))
+else :
+    print("1 또는 2만 입력해야 합니다.")
