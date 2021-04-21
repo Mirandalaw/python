ecd015acf559e40edd1bda586f352be5c7a72385
Mirandalaw <74170593+Mirandalaw@users.noreply.github.com>
2021-04-21 14:46:37 +0900

    Update User_Function.py

--- User_Function.py
+++ User_Function.py
@@ -53,3 +53,38 @@ print(r1,r2,r3)
 
 r4=reverse('a','b','c')
 print(r4)
+
+def Solve_Number1(Number):
+    behind_str="*******"
+    Behind_Phone_Number=(behind_str[0:]+Number[7:])
+    return Behind_Phone_Number
+
+# Phone_Number=input("")
+# B_P_N=Solve_Number1(Phone_Number)
+# print(B_P_N)
+#
+# Alphabet=input("")
+# The_Number=len(Alphabet)
+# last_Number=The_Number%5
+# mok=The_Number//5
+# i=0
+# while(i!=mok):
+#     print(Alphabet[(0+i*5):5+(i*5)])
+#     i=i+1
+# print(Alphabet[(5*mok):(5*mok+last_Number)])
+
+def F(n):
+    if n==1 :
+        return 1
+    if n==0 :
+        return 0
+    if n>1:
+        return F(n-1)+F(n-2)
+
+# Input_Num=int(input(""))
+# F(Input_Num)
+the_number=list(input(""))
+the_number.sort(reverse=True)
+l=len(the_number)
+answer= l-2
+print(the_number[0:answer])
