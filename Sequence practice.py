30675d7977fc5f5243e05c03de3fe8ee5add46bd
Mirandalaw <74170593+Mirandalaw@users.noreply.github.com>
2021-04-21 10:36:26 +0900

    Update Sequence practice.py

--- Sequence practice.py
+++ Sequence practice.py
@@ -1,16 +1,40 @@
-strdata ="Hello World!!"
-listdata=[1,2,[3,4,5]]
-print(strdata[4])
-print(strdata[-3])
-print(strdata[0])
-print(listdata[-1])
-print(listdata[2][-1])
-print(listdata[-1][-1])
-
-print(strdata[0:5])
-print(strdata[6:14])
-print(strdata[-8:])
-print(strdata[-3:])
-print(strdata[:])
-#시퀀스 자료형 슬라이싱[시작 인덱스: 끝 인덱스: 스텝]
-print(strdata[::2])
+# # strdata ="Hello World!!"
+# # listdata=[1,2,[3,4,5]]
+# # print(strdata[4])
+# # print(strdata[-3])
+# # print(strdata[0])
+# # print(listdata[-1])
+# # print(listdata[2][-1])
+# # print(listdata[-1][-1])
+# #
+# # print(strdata[0:5])
+# # print(strdata[6:14])
+# # print(strdata[-8:])
+# # print(strdata[-3:])
+# # print(strdata[:])
+# # #시퀀스 자료형 슬라이싱[시작 인덱스: 끝 인덱스: 스텝]
+# # print(strdata[::2])
+# #
+# # strdata1='hello World!!!'
+# # strdata2='My name is gildong'
+# # listdata1=['a','b','c',strdata1,strdata2]
+# # listdata2=[1,2,3,4,5,6]
+# #
+# # print(len(strdata1))
+# # print(len(strdata2))
+# # print(len(listdata1))
+# # print(len(listdata1[3]))
+# # print(len(listdata2))
+# 
+# listdata=[1,2,3,4]
+# ret1=5 in listdata
+# ret2=4 in listdata
+# 
+# strdata ='abcde'
+# ret3='c' in strdata
+# ret4='f' in strdata
+# 
+# print(ret1)
+# print(ret2)
+# print(ret3)
+# print(ret4)
