46176c6a187827c6918e67679c7993bbb653de22
Mirandalaw <74170593+Mirandalaw@users.noreply.github.com>
2021-04-21 10:47:17 +0900

    Add files via upload

---
+++ User_Function.py
@@ -0,0 +1,33 @@
+#n1,n2를 인자로 받아 더해주는 함수
+def add_number(n1,n2):
+    ret=n1+n2
+    return ret
+
+#t1,t2를 인자로 받아 더한 값만 보여주는 함수
+def add_text(t1,t2):
+    print(t1+t2)
+
+#전역변수
+param=10
+strdata="전역변수"
+
+def func1():
+    strdata="지역변수"
+    print(strdata)
+
+def func2(param):
+    param=1
+
+def func3():
+    global param
+    param=50
+
+func1()
+print(strdata)
+
+print(param)
+func2(param)
+print(param)
+
+func3()
+print(param)
