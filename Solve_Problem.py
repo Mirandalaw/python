cd6b9f26f727423f39865f34d0ced4c527ef7db0
Mirandalaw <74170593+Mirandalaw@users.noreply.github.com>
2021-04-22 12:39:20 +0900

    Add files via upload

--- Solve_Problem.py
+++ Solve_Problem.py
@@ -1,7 +1,19 @@
-from calculator import calculator
 
-arr=[5,9,7,10]
-divisor=5
-list1=[]
-list1.append(calculator(arr,divisor))
-print(list1)
+def Solve(Number):
+    dnumber=Number
+    d1number=0
+    answer=0
+    while Number>0:
+        d1number=d1number+(Number%10)
+        Number//=10
+    answer=dnumber+d1number
+    return answer
+
+Number=1000
+arr=[]
+for i in range(1,Number+1):
+    arr[Solve(i)]=1
+    print(Solve(i))
+for i in range(1,Number+1):
+    if(arr[i]!=1):
+        print(i)
